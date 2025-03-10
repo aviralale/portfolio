9a397b7a0e55004b2f5e9db3d0e6d1a89c51d003
Aviral Ale <aviralale@gmail.com>
2025-03-10 21:14:37 +0545

    removed blog link in navbar

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -30,7 +30,7 @@ export const DATA = {
   ],
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
-    { href: "/blog", icon: NotebookIcon, label: "Blog" },
+    // { href: "/blog", icon: NotebookIcon, label: "Blog" },
   ],
   contact: {
     email: "aviralale@gmail.com",
